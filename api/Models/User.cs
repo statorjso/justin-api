2722075fdb3731299602f0e3f7c1f50bdb11f6f9
RTI\joh <joh@rti.org>
2022-08-23 13:59:35 -0400

    Functional test API methods

--- api/Models/User.cs
+++ api/Models/User.cs
@@ -8,7 +8,7 @@ namespace api.Models
     public class User
     {
         public int UserId { get; set; }
-        public string Username { get; internal set; }
-        public string Password { get; internal set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
     }
 }
